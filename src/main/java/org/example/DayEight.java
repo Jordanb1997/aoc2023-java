173b9f1c4c402cdeda01e2bd099e9aa470cf86e8
Jordan Bannister <40512499+Jordanb1997@users.noreply.github.com>
2023-12-15 10:05:48 +1100

    day 2

---
+++ src/main/java/org/example/DayEight.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class DayEight {
+
+    /**
+     Example output: RL
+
+     Example network:
+     AAA = (BBB, CCC)
+     BBB = (DDD, EEE)
+     CCC = (ZZZ, GGG)
+     DDD = (DDD, DDD)
+     EEE = (EEE, EEE)
+     GGG = (GGG, GGG)
+     ZZZ = (ZZZ, ZZZ)
+
+
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/example/DayNine.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class DayNine {
+    public static void main(String[] args) {
+
+    }
+}
